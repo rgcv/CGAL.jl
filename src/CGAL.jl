6312153121c51d9c2a742d31868679772df82947
Rui Ventura <rventura.pt@outlook.com>
2020-01-31 10:59:46 +0000

    Drop tentative support for julia <1.3

--- src/CGAL.jl
+++ src/CGAL.jl
@@ -1,15 +1,5 @@
 module CGAL
 
-if VERSION < v"1.3.0-rc4" # pre artifacts
-    const depsfile = joinpath(dirname(@__DIR__), "deps", "deps.jl")
-    if !isfile(depsfile)
-        error("$depsfile not present, try rebuilding CGAL.jl")
-    end
-    include(depsfile)
-else
-    using libcgal_julia_jll
-end
-
 struct Origin end; const ORIGIN = Origin()
 struct NullVector end; const NULL_VECTOR = NullVector()
 
@@ -20,6 +10,7 @@ struct Scaling end; const SCALING = Scaling()
 struct Translation end; const TRANSLATION = Translation()
 
 using CxxWrap
+using libcgal_julia_jll
 
 # useful for development: indicate full path to wrapper lib using the
 # JLCGAL_LIBPATH environment variable before loading the module
