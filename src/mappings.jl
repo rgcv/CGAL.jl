e9e3cb9f0ffd6cb446fc458a9fe68da91d1915ea
Rui Ventura <rventura.pt@outlook.com>
2020-02-10 19:56:01 +0000

    Add aforementioned/included mappings file

---
+++ src/mappings.jl
@@ -0,0 +1,10 @@
+# Origin constants
+struct Origin end; const ORIGIN = Origin()
+struct NullVector end; const NULL_VECTOR = NullVector()
+
+# Transformation tags
+struct IdentityTransformation end; const IDENTITY = IdentityTransformation()
+struct Reflection end; const REFLECTION = Reflection()
+struct Rotation end; const ROTATION = Rotation()
+struct Scaling end; const SCALING = Scaling()
+struct Translation end; const TRANSLATION = Translation()
